bee65d16d630406cf59287557a218a956f8f3f41
Nora <nora@Noras-MacBook-Pro.local>
2022-12-17 21:57:00 +0300

    kubernetes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="noradoss/myproject"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run myproject --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward myproject 8000:80
